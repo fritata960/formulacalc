3e1b322c7b05e1875776df55bb37c9166aaab478
fritata960 <148252456+fritata960@users.noreply.github.com>
2023-10-17 10:25:53 -0600

    revision 1
    base value edit function added for percentage values, while loop functionality added.

--- src/Main.java
+++ src/Main.java
@@ -20,40 +20,43 @@ public class Main {
             float n = reader.nextFloat();
             weightArr[i] = n;
         }*/
-        for(int i=0; i<weightArr.length;i++){ //prints each individual weight value in order
-            System.out.print(weightArr[i] + " ");
-        }
+       
         System.out.println();
         float percT = 0;
         for(int i = 0; i<weightArr.length; i++){ //generates corresponding percentage array
             percT = (weightArr[i]/finalW)*100;
             percArr[i] = percT;
         }
-
-        for(int i=0; i<percArr.length;i++){ //prints each individual percentage value in order
-            System.out.print(percArr[i] + "%, ");
-        }
+        printer(percArr, weightArr);
 
         while (useSel != 0){
             System.out.println("next selection: 1(editPercent) 2(editWeight) 0(exit)");
             useSel = reader.nextInt();
             System.out.println("selected: " + useSel);
-            if(useSel == 1){
+            if(useSel == 1){ //selection 1, edits percentage values of percarray and regens weightarray
                 System.out.println("ingredient percent index to edit:");
                 int epI = reader.nextInt();
-                System.out.println("enter replacement value (as decimal):");
-                percArr[epI-1] = reader.nextFloat();
-                percArr[0] = 100 - nonBaseSum(percArr);
-                weightArr[epI-1] = (percArr[epI-1]/100)*finalW;
-                weightArr[0] = (percArr[0]/100)*finalW;
-                for(int i=0; i<percArr.length;i++){ //prints each individual percentage value in order
-                    System.out.print(percArr[i] + "%, ");
+                if(epI == 1){//in the case of a base value edit get confirmation and redir to the base value edit function
+                    System.out.println("This edits the BASE value and changes all other component weights to equal the final weight. Are you sure? 1:Y 2:N");
+                    useSel = reader.nextInt();
+                    if(useSel == 1){
+                        System.out.println("enter replacement value (as decimal):");
+                        float epOneV = reader.nextFloat();
+                        float[][] st = basePValEdit(epOneV, finalW, percArr, weightArr);//base value edit function, outputs 2d array of float arrays for regen'd percArr and weightArr
+                        percArr = st[0];
+                        weightArr = st[1];
+                        printer(percArr, weightArr);
+                    }
                 }
-                System.out.println();
-                for(int i=0; i<weightArr.length;i++){ //prints each individual weight value in order
-                    System.out.print(weightArr[i] + ", ");
+                else{
+                    System.out.println("enter replacement value (as decimal):");
+                    percArr[epI-1] = reader.nextFloat();
+                    percArr[0] = 100 - nonBaseSum(percArr);
+                    weightArr[epI-1] = (percArr[epI-1]/100)*finalW;
+                    weightArr[0] = (percArr[0]/100)*finalW;
+                    printer(percArr, weightArr);
                 }
-                System.out.println();
+
             }
             if(useSel == 2){
                 System.out.println("ingredient weight index to edit:");
@@ -65,13 +68,7 @@ public class Main {
                     percT = (weightArr[i]/finalW)*100;
                     percArr[i] = percT;
                 }
-                for(int i=0; i<percArr.length;i++){ //prints each individual percentage value in order
-                    System.out.print(percArr[i] + "%, ");
-                }
-                System.out.println();
-                for(int i=0; i<weightArr.length;i++){ //prints each individual weight value in order
-                    System.out.print(weightArr[i] + " ");
-                }
+                printer(percArr, weightArr);
                 System.out.println();
             }
 
@@ -85,7 +82,6 @@ public class Main {
         for(int i = 1; i< a.length; i++){
             s = s+a[i];
         }
-        System.out.println("sumdebug" + s);
         return s;
     }
     public static float Sum(float[] a){//returns sum of every element
@@ -95,17 +91,21 @@ public class Main {
         }
         return s;
     }
-    public static float[] baseValEdit(float nV, float fW, float[] perc, float[] weight){
-        float[] r = new float[weight.length];
-        float nBP = (nV/fW)*100;//declare inscope values and set equal to edited values, % and W
-        float nBW = nBP * fW;
-        float nDif = nBP/perc.length;
+    public static float[][] basePValEdit(float nV, float fW, float[] perc, float[] weight){//no dual returns in java :weirdchamp:
+        float[][] r = new float[2][];//declare inscope values and set equal to edited values, % and W
+        float nBW = (nV/100) * fW;
+        float nDif = (perc[0]-nV)/(perc.length-1);
+        perc[0] = nV;
+
         for(int i = 1; i< perc.length; i++){
             perc[i] += nDif;
         }
-        
-
-
+        weight[0] = nBW;
+        for(int i = 1; i<weight.length; i++){
+            weight[i] = fW * (perc[i]/100);
+        }
+        r[0] = perc;
+        r[1] = weight;
         return r;
     }
     public static float[] uInParse(String a){//parses input from comma separated string to float array
@@ -116,5 +116,15 @@ public class Main {
             r[i] = Float.parseFloat(preConVal[i]);
         }
         return r;
+    }
+    public static void printer(float[] percArr, float[] weightArr){
+        for(int i=0; i<percArr.length;i++){ //prints each individual percentage value in order
+            System.out.print(percArr[i] + "%, ");
+        }
+        System.out.println();
+        for(int i=0; i<weightArr.length;i++){ //prints each individual weight value in order
+            System.out.print(weightArr[i] + ", ");
+        }
+        System.out.println();
     }
 }
